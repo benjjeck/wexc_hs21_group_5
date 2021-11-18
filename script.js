a5a4ad68107acc66b7b2c40bf633f9cdb4ec151e
Jonas M <jonas.moeller@students.fhnw.ch>
2021-11-18 21:18:33 +0100

    first implementation of improved design (incl. round corners)

--- script.js
+++ script.js
@@ -120,36 +120,25 @@ const formatTimeString = (date) => {
 };
 
 const drawBackground = () => {
+
+
   // rectangles
-  const rectHeight = canvas.height / 4;
+  const rectHeight = canvas.height / 5;
 
   // clear canvas
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  //upper Rect
+  //backgroundRect
   ctx.beginPath();
-  ctx.fillStyle = "#B7D7A6";
-  ctx.fillRect(80, 0, 1000, canvas.height * 0.25);
-  ctx.stroke();
-
-  //bottom Rect
-  ctx.beginPath();
-  ctx.fillStyle = "#B7D7A6";
-  ctx.fillRect(80, canvas.height * 0.75, 1000, canvas.height * 0.25);
-  ctx.stroke();
-
-  // Middle Rect for time
-  ctx.beginPath();
-  ctx.lineWidth = "3";
-  ctx.fillStyle = "#E9E9E9";
-  ctx.fillRect(80, canvas.height * 0.25, 1000, canvas.height * 0.5);
+  ctx.fillStyle = "#CFF0C2";
+  roundRect(80, 0, 1000, canvas.height,20,true,false);
   ctx.stroke();
 
   //left Pane for time
   ctx.beginPath();
   ctx.lineWidth = "3";
   ctx.fillStyle = "#BFEDF6";
-  ctx.fillRect(80, 0, 30, canvas.height);
+  roundRect(80, 0, 30, canvas.height,8,true,false);
   ctx.stroke();
   ctx.fillStyle = "#565656";
 
@@ -159,10 +148,10 @@ const drawBackground = () => {
   ctx.fontWeight = "50";
 
   ctx.fillStyle = "#E0E0E0";
-  ctx.fillRect(22, 87, 50, 30);
-  ctx.fillRect(22, 136, 50, 30);
-  ctx.fillRect(22, 187, 50, 30);
-  ctx.fillRect(22, 247, 50, 30);
+  roundRect(22, 87, 50, 30,8,true,false);
+  roundRect(22, 136, 50, 30,8,true,false);
+  roundRect(22, 187, 50, 30,8,true,false);
+  roundRect(22, 247, 50, 30,8,true,false);
 
   ctx.font = "22px Sans-Serif";
   ctx.textAlign = "start";
@@ -176,7 +165,7 @@ const drawBackground = () => {
 
   //marked timestamp
   ctx.fillStyle = "#BE3527";
-  ctx.fillRect(22, 22, 50, 30);
+  roundRect(22, 22, 50, 30,8,true,false);
   ctx.fillStyle = "white";
   ctx.fillText("4h", 35, 45);
 
@@ -187,15 +176,12 @@ const drawBackground = () => {
   ctx.strokeStyle = "#BE3429";
   ctx.strokeRect(80, 0, 30, rectHeight);
 
-  //2h, 1h and 30m for thirds
-  const thirdRects = (rectHeight / 3) * 2;
-
   ctx.strokeStyle = "#ABABAB";
   ctx.lineWidth = 2;
-  ctx.strokeRect(80, rectHeight, 30, thirdRects);
-  ctx.strokeRect(80, rectHeight + thirdRects, 30, thirdRects);
-  ctx.strokeRect(80, rectHeight + thirdRects * 2, 30, thirdRects);
-  ctx.strokeRect(80, rectHeight * 3, 30, rectHeight);
+  ctx.strokeRect(80, rectHeight, 30, rectHeight);
+  ctx.strokeRect(80, rectHeight*2, 30, rectHeight);
+  ctx.strokeRect(80, rectHeight*3, 30, rectHeight);
+  ctx.strokeRect(80, rectHeight * 4, 30, rectHeight);
 
   // borders
   ctx.strokeStyle = "#1D598F";
@@ -205,6 +191,66 @@ const drawBackground = () => {
 
   ctx.stroke();
 };
+
+// Thanks Juan Mendes
+// https://stackoverflow.com/questions/1255512/how-to-draw-a-rounded-rectangle-using-html-canvas
+
+/**
+ * Draws a rounded rectangle using the current state of the canvas.
+ * If you omit the last three params, it will draw a rectangle
+ * outline with a 5 pixel border radius
+ * @param {Number} x The top left x coordinate
+ * @param {Number} y The top left y coordinate
+ * @param {Number} width The width of the rectangle
+ * @param {Number} height The height of the rectangle
+ * @param {Number} [radius = 5] The corner radius; It can also be an object
+ *                 to specify different radii for corners
+ * @param {Number} [radius.tl = 0] Top left
+ * @param {Number} [radius.tr = 0] Top right
+ * @param {Number} [radius.br = 0] Bottom right
+ * @param {Number} [radius.bl = 0] Bottom left
+ * @param {Boolean} [fill = false] Whether to fill the rectangle.
+ * @param {Boolean} [stroke = true] Whether to stroke the rectangle.
+ */
+const roundRect = (x, y, width, height, radius, fill, stroke) => {
+  if (typeof stroke === "undefined") {
+    stroke = true;
+  }
+  if (typeof radius === "undefined") {
+    radius = 5;
+  }
+  if (typeof radius === "number") {
+    radius = { tl: radius, tr: radius, br: radius, bl: radius };
+  } else {
+    var defaultRadius = { tl: 0, tr: 0, br: 0, bl: 0 };
+    for (var side in defaultRadius) {
+      radius[side] = radius[side] || defaultRadius[side];
+    }
+  }
+  ctx.beginPath();
+  ctx.moveTo(x + radius.tl, y);
+  ctx.lineTo(x + width - radius.tr, y);
+  ctx.quadraticCurveTo(x + width, y, x + width, y + radius.tr);
+  ctx.lineTo(x + width, y + height - radius.br);
+  ctx.quadraticCurveTo(
+      x + width,
+      y + height,
+      x + width - radius.br,
+      y + height
+  );
+  ctx.lineTo(x + radius.bl, y + height);
+  ctx.quadraticCurveTo(x, y + height, x, y + height - radius.bl);
+  ctx.lineTo(x, y + radius.tl);
+  ctx.quadraticCurveTo(x, y, x + radius.tl, y);
+  ctx.closePath();
+  if (fill) {
+    ctx.fill();
+  }
+  if (stroke) {
+    ctx.stroke();
+  }
+};
+
 
 const getMousePos = (canvas, evt) => {
   var rect = canvas.getBoundingClientRect();
@@ -263,64 +309,7 @@ window.addEventListener("mousemove", handleMouseMove, false);
 window.addEventListener("click", handleClick, false);
 
 const addToolTip = (x, y, text) => {
-  // Thanks Juan Mendes
-  // https://stackoverflow.com/questions/1255512/how-to-draw-a-rounded-rectangle-using-html-canvas
 
-  /**
-   * Draws a rounded rectangle using the current state of the canvas.
-   * If you omit the last three params, it will draw a rectangle
-   * outline with a 5 pixel border radius
-   * @param {Number} x The top left x coordinate
-   * @param {Number} y The top left y coordinate
-   * @param {Number} width The width of the rectangle
-   * @param {Number} height The height of the rectangle
-   * @param {Number} [radius = 5] The corner radius; It can also be an object
-   *                 to specify different radii for corners
-   * @param {Number} [radius.tl = 0] Top left
-   * @param {Number} [radius.tr = 0] Top right
-   * @param {Number} [radius.br = 0] Bottom right
-   * @param {Number} [radius.bl = 0] Bottom left
-   * @param {Boolean} [fill = false] Whether to fill the rectangle.
-   * @param {Boolean} [stroke = true] Whether to stroke the rectangle.
-   */
-  const roundRect = (x, y, width, height, radius, fill, stroke) => {
-    if (typeof stroke === "undefined") {
-      stroke = true;
-    }
-    if (typeof radius === "undefined") {
-      radius = 5;
-    }
-    if (typeof radius === "number") {
-      radius = { tl: radius, tr: radius, br: radius, bl: radius };
-    } else {
-      var defaultRadius = { tl: 0, tr: 0, br: 0, bl: 0 };
-      for (var side in defaultRadius) {
-        radius[side] = radius[side] || defaultRadius[side];
-      }
-    }
-    ctx.beginPath();
-    ctx.moveTo(x + radius.tl, y);
-    ctx.lineTo(x + width - radius.tr, y);
-    ctx.quadraticCurveTo(x + width, y, x + width, y + radius.tr);
-    ctx.lineTo(x + width, y + height - radius.br);
-    ctx.quadraticCurveTo(
-      x + width,
-      y + height,
-      x + width - radius.br,
-      y + height
-    );
-    ctx.lineTo(x + radius.bl, y + height);
-    ctx.quadraticCurveTo(x, y + height, x, y + height - radius.bl);
-    ctx.lineTo(x, y + radius.tl);
-    ctx.quadraticCurveTo(x, y, x + radius.tl, y);
-    ctx.closePath();
-    if (fill) {
-      ctx.fill();
-    }
-    if (stroke) {
-      ctx.stroke();
-    }
-  };
 };
 
 // Returns a function, that, as long as it continues to be invoked, will not
